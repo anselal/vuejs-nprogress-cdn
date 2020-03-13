c3a09cf8d5572b68a3cf8aa6b31a044723847108
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2020-03-14 00:44:39 +0200

    :up: update(homeView): Move about link to the right

--- app/views/homeView.js
+++ app/views/homeView.js
@@ -3,9 +3,9 @@ homeTemplate = `
   <div>
 	<div class="w3-bar w3-black">
       <router-link to="/" exact-active-class="w3-blue" class="w3-bar-item w3-button">Home</router-link>
-      <router-link to="/about" exact-active-class="w3-blue" class="w3-bar-item w3-button">About</router-link>
       <router-link to="/breeds" exact-active-class="w3-blue" class="w3-bar-item w3-button">Breeds</router-link>
-      <router-link to="/contact" exact-active-class="w3-blue" class="w3-bar-item w3-button">Contact</router-link>
+	  <router-link to="/contact" exact-active-class="w3-blue" class="w3-bar-item w3-button">Contact</router-link>
+	  <router-link to="/about" exact-active-class="w3-blue" class="w3-bar-item w3-button w3-right">About</router-link>
 	</div>
 	<div class="w3-container">
 	  <h1>Home View</h1>
