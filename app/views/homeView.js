31247058db02a1503fcd78dca33d2fa0b8e09055
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2020-03-19 22:44:29 +0200

    :star: add(sidebar)

--- app/views/homeView.js
+++ app/views/homeView.js
@@ -1,23 +1,37 @@
 homeTemplate = `
 <template id='home-template'>
   <div>
+    <!-- Top bar -->
     <div class="w3-bar w3-black">
+      <button class="w3-button w3-green w3-hide-large w3-left" onclick="toggle_sidebar('block')">&#9776;</button>
       <router-link to="/" exact-active-class="w3-blue" class="w3-bar-item w3-button">Home</router-link>
       <router-link to="/breeds" exact-active-class="w3-blue" class="w3-bar-item w3-button">Breeds</router-link>
       <router-link to="/contact" exact-active-class="w3-blue" class="w3-bar-item w3-button">Contact</router-link>
       <router-link to="/about" exact-active-class="w3-blue" class="w3-bar-item w3-button w3-right">About</router-link>
     </div>
-    <div class="w3-container">
-      <h1>Home View</h1>
+
+    <!-- Sidebar -->
+    <div class="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left" style="width:200px;z-index:5" id="mySidebar">
+      <button class="w3-bar-item w3-button w3-hide-large"
+      onclick="toggle_sidebar('none')">Close &times;</button>
+      <a href="#" class="w3-bar-item w3-button">Link 1</a>
+      <a href="#" class="w3-bar-item w3-button">Link 2</a>
+      <a href="#" class="w3-bar-item w3-button">Link 3</a>
     </div>
-    <div class="w3-container">
-      <transition name="slide-fade" mode="out-in">
-        <keep-alive>
-            <router-view></router-view>
-        </keep-alive>
-      </transition>
-    </div>
-    <p v-if="breeds">{{ breeds.userId }}, {{ breeds.id }}</p>
+
+    <!-- Overlay -->
+    <div class="w3-overlay" onclick="toggle_sidebar('none')" style="cursor:pointer" id="myOverlay"></div>
+
+    <div class="w3-main" style="margin-left:200px">
+      <div class="w3-container">
+        <h1>Home View</h1>
+        <transition name="slide-fade" mode="out-in">
+          <keep-alive>
+              <router-view></router-view>
+          </keep-alive>
+        </transition>
+      </div>
+      <p v-if="breeds">{{ breeds.userId }}, {{ breeds.id }}</p>
   </div>
 </template>
 `;
@@ -44,4 +58,9 @@ const HomeView = {
       this.ax();
     }, 2000);
   }
+}
+
+function toggle_sidebar(style) {
+  document.getElementById("mySidebar").style.display = style;
+  document.getElementById("myOverlay").style.display = style;
 }
